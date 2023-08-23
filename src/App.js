cd6f41994be3fcba22439af6217c4740df2a0d0b
Grazziano Fagundes <grazzianofagundes@gmail.com>
2023-08-23 11:36:21 -0300

    Configuring routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+import RoutesApp from './routes';
 
 function App() {
   return (
-    <div>
-      <h1>TESTE</h1>
-    </div>
+    <BrowserRouter>
+      <RoutesApp />
+    </BrowserRouter>
   );
 }
 
