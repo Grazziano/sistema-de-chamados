d8c8828007a8096293d81baeb339072becb042eb
Grazziano Fagundes <grazzianofagundes@gmail.com>
2023-08-24 11:46:04 -0300

    User login

--- src/contexts/auth.js
+++ src/contexts/auth.js
@@ -1,6 +1,9 @@
 import { useState, createContext, useEffect } from 'react';
 import { auth, db } from '../services/firebaseConnection';
-import { createUserWithEmailAndPassword } from 'firebase/auth';
+import {
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+} from 'firebase/auth';
 import { doc, getDoc, setDoc } from 'firebase/firestore';
 
 import { useNavigate } from 'react-router-dom';
@@ -14,10 +17,34 @@ function AuthProvider({ children }) {
 
   const navigate = useNavigate();
 
-  function signIn(email, password) {
-    console.log(email);
-    console.log(password);
-    console.log('LOGADO COM SUCESSO');
+  async function signIn(email, password) {
+    setLoadingAuth(true);
+
+    await signInWithEmailAndPassword(auth, email, password)
+      .then(async (value) => {
+        let uid = value.user.uid;
+
+        const docRef = doc(db, 'users', uid);
+        const docSnap = await getDoc(docRef);
+
+        let data = {
+          uid: uid,
+          name: docSnap.data().name,
+          email: value.user.email,
+          avatarUrl: docSnap.data().avatarUrl,
+        };
+
+        setUser(data);
+        storageUser(data);
+        setLoadingAuth(false);
+        toast.success('Bem vindo(a) de volta!');
+        navigate('/dashboard');
+      })
+      .catch((error) => {
+        setLoadingAuth(false);
+        console.log(error);
+        toast.error('Ops algo deu errado!');
+      });
   }
 
   async function signUp(email, password, name) {
