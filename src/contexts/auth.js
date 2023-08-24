880c95be4cc90df0a76b854be86aeecc68b3a055
Grazziano Fagundes <grazzianofagundes@gmail.com>
2023-08-24 11:23:21 -0300

    Registering user

--- src/contexts/auth.js
+++ src/contexts/auth.js
@@ -1,14 +1,46 @@
 import { useState, createContext, useEffect } from 'react';
+import { auth, db } from '../services/firebaseConnection';
+import { createUserWithEmailAndPassword } from 'firebase/auth';
+import { doc, getDoc, setDoc } from 'firebase/firestore';
 
 export const AuthContext = createContext({});
 
 function AuthProvider({ children }) {
   const [user, setUser] = useState(null);
+  const [loadingAuth, setLoadingAuth] = useState(false);
 
   function signIn(email, password) {
     console.log(email);
     console.log(password);
     console.log('LOGADO COM SUCESSO');
+  }
+
+  async function signUp(email, password, name) {
+    setLoadingAuth(true);
+
+    await createUserWithEmailAndPassword(auth, email, password)
+      .then(async (value) => {
+        let uid = value.user.uid;
+
+        await setDoc(doc(db, 'users', uid), {
+          name: name,
+          avatarUrl: null,
+        }).then(() => {
+          let data = {
+            uid: uid,
+            name: name,
+            email: value.user.email,
+            avatarUrl: null,
+          };
+
+          setUser(data);
+          setLoadingAuth(false);
+        });
+      })
+      .catch((error) => {
+        console.log(error);
+        setLoadingAuth(false);
+      });
   }
 
   return (
@@ -17,6 +49,8 @@ function AuthProvider({ children }) {
         signed: !!user,
         user,
         signIn,
+        signUp,
+        loadingAuth,
       }}
     >
       {children}
