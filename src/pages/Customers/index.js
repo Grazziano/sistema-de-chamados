8f501af86d792d56d4553ebd29f5ec794041578b
Grazziano Fagundes <grazzianofagundes@gmail.com>
2023-08-26 19:45:12 -0300

    Registering client

--- src/pages/Customers/index.js
+++ src/pages/Customers/index.js
@@ -3,15 +3,38 @@ import Header from '../../components/Header';
 import Title from '../../components/Title';
 import { FiUser } from 'react-icons/fi';
 
+import { db } from '../../services/firebaseConnection';
+import { addDoc, collection } from 'firebase/firestore';
+
+import { toast } from 'react-toastify';
+
 export default function Customers() {
   const [name, setName] = useState('');
   const [cnpj, setCnpj] = useState('');
   const [address, setAddress] = useState('');
 
-  function handleRegister(e) {
+  async function handleRegister(e) {
     e.preventDefault();
 
-    alert('TESTE');
+    if (name !== '' && cnpj !== '' && address !== '') {
+      await addDoc(collection(db, 'customers'), {
+        fantasyName: name,
+        cnpj: cnpj,
+        address: address,
+      })
+        .then(() => {
+          setName('');
+          setCnpj('');
+          setAddress('');
+          toast.success('Empresa registrada!');
+        })
+        .catch((error) => {
+          console.log(error);
+          toast.error('Erro ao fazer o cadastro!');
+        });
+    } else {
+      toast.error('Preencha todos os campos!');
+    }
   }
 
   return (
