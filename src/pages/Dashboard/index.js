f0789a5bc174368bb257b8d6b9f0b7dc3a7c2f04
Grazziano Fagundes <grazzianofagundes@gmail.com>
2023-08-26 20:02:05 -0300

    Dashboard page

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -2,6 +2,12 @@ import React, { useContext } from 'react';
 import { AuthContext } from '../../contexts/auth';
 
 import Header from '../../components/Header';
+import Title from '../../components/Title';
+import { FiEdit2, FiMessageSquare, FiPlus, FiSearch } from 'react-icons/fi';
+
+import { Link } from 'react-router-dom';
+
+import './dashboard.css';
 
 export default function Dashboard() {
   const { logout } = useContext(AuthContext);
@@ -13,8 +19,53 @@ export default function Dashboard() {
   return (
     <div>
       <Header />
-      <h1>Página Dashboard</h1>
-      <button onClick={handleLogout}>Sair da conta</button>
+
+      <div className="content">
+        <Title name="Tickets">
+          <FiMessageSquare size={25} />
+        </Title>
+
+        <>
+          <Link to="/new" className="new">
+            <FiPlus color="#FFF" size={25} />
+            Novo chamado
+          </Link>
+
+          <table>
+            <thead>
+              <tr>
+                <th scope="col">Cliente</th>
+                <th scope="col">Assunto</th>
+                <th scope="col">Status</th>
+                <th scope="col">Cadastrado em</th>
+                <th scope="col">#</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td data-label="Cliente">Mercado esquina</td>
+                <td data-label="Assunto">Suporte</td>
+                <td data-label="Status">Em Aberto</td>
+                <td data-label="Cadastrado">12/05/2022</td>
+                <td data-label="#">
+                  <button
+                    className="action"
+                    style={{ backgroundColor: '#3583f6' }}
+                  >
+                    <FiSearch color="#FFF" size={17} />
+                  </button>
+                  <button
+                    className="action"
+                    style={{ backgroundColor: '#F6A935' }}
+                  >
+                    <FiEdit2 color="#FFF" size={17} />
+                  </button>
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </>
+      </div>
     </div>
   );
 }
