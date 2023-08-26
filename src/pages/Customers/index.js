67868e0de675eb994ce5c2059d433c23b1078516
Grazziano Fagundes <grazzianofagundes@gmail.com>
2023-08-26 19:35:37 -0300

    Clients page

---
+++ src/pages/Customers/index.js
@@ -0,0 +1,58 @@
+import React, { useState } from 'react';
+import Header from '../../components/Header';
+import Title from '../../components/Title';
+import { FiUser } from 'react-icons/fi';
+
+export default function Customers() {
+  const [name, setName] = useState('');
+  const [cnpj, setCnpj] = useState('');
+  const [address, setAddress] = useState('');
+
+  function handleRegister(e) {
+    e.preventDefault();
+
+    alert('TESTE');
+  }
+
+  return (
+    <div>
+      <Header />
+
+      <div className="content">
+        <Title name="Clientes">
+          <FiUser size={25} />
+        </Title>
+
+        <div className="container">
+          <form className="form-profile" onSubmit={handleRegister}>
+            <label>Nome fantasia</label>
+            <input
+              type="text"
+              placeholder="Nome da empresa"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+            />
+
+            <label>CNPJ</label>
+            <input
+              type="text"
+              placeholder="Digite o CNPJ"
+              value={cnpj}
+              onChange={(e) => setCnpj(e.target.value)}
+            />
+
+            <label>Endereço</label>
+            <input
+              type="text"
+              placeholder="Endereço da empresa"
+              value={address}
+              onChange={(e) => setAddress(e.target.value)}
+            />
+
+            <button type="submit">Salvar</button>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+}
